334fefe7f4b31c839ca6e2fe82c9f456569ffec0
Matias Lopez <mlapente96@gmail.com>
2022-04-17 17:03:33 -0300

    Changes in classes and lwc

--- force-app/main/default/lwc/serviceComponent/serviceComponent.js
+++ force-app/main/default/lwc/serviceComponent/serviceComponent.js
@@ -1,13 +1,17 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import  executeService  from '@salesforce/apex/ucl_service.executeService';
 
 export default class ServiceComponent extends LightningElement {
+         
     handleClick(){
-        const toastEvent = new ShowToastEvent({
-            title: "Service invoked",
-            message: " ",
-            variant: "success"
-        });
-        this.dispatchEvent(toastEvent);
-    }
+        executeService()
+            .then(() => {
+                this.ShowToastEvent("Success!", "Success", "No error!");
+            })
+            .catch(error =>{
+                this.ShowToastEvent("Error!", "Error", "There is an error somewhere");
+                this.error = error
+            })
+        }
 }
