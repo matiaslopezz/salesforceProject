493610581de830b19b059d3523067e80a0e99c21
Matias Lopez <mlapente96@gmail.com>
2022-04-18 20:34:12 -0300

    Improving classes and LWC

--- force-app/main/default/lwc/serviceComponent/serviceComponent.js
+++ force-app/main/default/lwc/serviceComponent/serviceComponent.js
@@ -1,9 +1,9 @@
 import { LightningElement } from 'lwc';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import  executeService  from '@salesforce/apex/ucl_service.executeService';
+import clearDatabase from '@salesforce/apex/ucl_clearDatabase.clearDatabase';
 
 export default class ServiceComponent extends LightningElement {
-
     handleClick(){
         executeService()
         .then(result => {
@@ -12,6 +12,7 @@ export default class ServiceComponent extends LightningElement {
                 message: "No errors",
                 variant: "success"
             });
+            this.dispatchEvent(toastEvent);
         })
         .catch(error => {
             const toastEvent = new ShowToastEvent({
@@ -19,7 +20,27 @@ export default class ServiceComponent extends LightningElement {
                 message: "Errors",
                 variant: "failure"
             });
+            this.dispatchEvent(toastEvent);
+        });        
+    }
+
+    clearClick(){
+        clearDatabase()
+        .then(result => {
+            const toastEvent = new ShowToastEvent({
+                title: "Database successfully cleared",
+                message: "No errors",
+                variant: "success"
+            });
+            this.dispatchEvent(toastEvent);
+        })
+        .catch(error => {
+            const toastEvent = new ShowToastEvent({
+                title: "Database still alive",
+                message: "Errors",
+                variant: "failure"
+            });
+            this.dispatchEvent(toastEvent);
         });
-        this.dispatchEvent(toastEvent);
     }
 }
