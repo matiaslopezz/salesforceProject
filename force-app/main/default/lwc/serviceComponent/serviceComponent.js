93e1895652f4f70a858ee5a747b9742ba1a4e484
Matias Lopez <mlapente96@gmail.com>
2022-04-18 10:29:45 -0300

    Changes in classes and lwc v3

--- force-app/main/default/lwc/serviceComponent/serviceComponent.js
+++ force-app/main/default/lwc/serviceComponent/serviceComponent.js
@@ -1,4 +1,5 @@
 import { LightningElement } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import  executeService  from '@salesforce/apex/ucl_service.executeService';
 
 export default class ServiceComponent extends LightningElement {
@@ -6,10 +7,19 @@ export default class ServiceComponent extends LightningElement {
     handleClick(){
         executeService()
         .then(result => {
-
+            const toastEvent = new ShowToastEvent({
+                title: "Service successfully fired",
+                message: "No errors",
+                variant: "success"
+            });
         })
         .catch(error => {
-
-        })
+            const toastEvent = new ShowToastEvent({
+                title: "Service did not run properly",
+                message: "Errors",
+                variant: "failure"
+            });
+        });
+        this.dispatchEvent(toastEvent);
     }
 }
