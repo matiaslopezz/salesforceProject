fcac1e503e5e14485f58c1003b1872d2c36ba5ea
Matias Lopez <mlapente96@gmail.com>
2022-04-19 16:28:37 -0300

    LWC Image and Test

---
+++ force-app/main/default/lwc/lawyerImage/lawyerImage.js
@@ -0,0 +1,6 @@
+import { LightningElement } from 'lwc';
+import My_Resource from '@salesforce/resourceUrl/myResource';
+
+export default class LawyerImage extends LightningElement {
+    myResource = My_Resource;
+}
